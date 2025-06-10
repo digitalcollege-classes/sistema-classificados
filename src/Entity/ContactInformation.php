b71783cd4dd96d240f206ece05410df08919f820
jgaguiarm <jaimegaguiar@gmail.com>
2025-06-10 18:46:25 -0300

    Criando a entidade ContactInformation

---
+++ src/Entity/ContactInformation.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use DateTime;
+
+class ContactInformation
+{
+    private int $id;
+
+    private ?string $phone;
+
+    private ?string $email;
+
+    private ?string $socialMedia;
+
+    private DateTime $createdAt;
+
+    private DateTime $updatedAt;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): void
+    {
+        $this->phone = $phone;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getSocialMedia(): string
+    {
+        return $this->socialMedia;
+    }
+
+    public function setSocialMedia(string $socialMedia): void
+    {
+        $this->name = $socialMedia;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+}
