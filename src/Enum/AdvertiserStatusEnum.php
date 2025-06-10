448dd35867f025d45c24a73eb07e6d770ba471a0
Allyson Gerald <allyson.gerald@gmail.com>
2025-06-10 18:31:08 -0300

    feat: criado entidade advertisement (#73)

--- src/Enum/AdvertiserStatusEnum.php
+++ src/Enum/AdvertiserStatusEnum.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace App\Enum;
 
-enum AdvertisementStatusEnum: string
+enum AdvertiserStatusEnum: string
 {
     case ACTIVE = 'ACTIVE';
     case INACTIVE = 'INACTIVE';
+    case DRAFT = 'DRAFT';
+    case PUBLISHED = 'PUBLISHED';
     case PAUSED = 'PAUSED';
-    case SOLD = 'SOLD';
+    case TRASH = 'TRASH';
 }
