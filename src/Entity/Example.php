fb69e606fc24033ab84a4d0fb1ffe0924eba7b36
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2025-06-10 19:51:12 -0300

    Cria exemplo de mapeamento de entidade com ORM

--- src/Entity/Example.php
+++ src/Entity/Example.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
 class Example
 {
+    #[ORM\Id] #[ORM\Column] #[ORM\GeneratedValue]
+    private int $id;
+
+    #[ORM\Column(length: 100, nullable: true)]
     private ?string $name;
 
+    #[ORM\Column]
     private bool $status = false;
 
     public function getName(): string
