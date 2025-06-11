d8550653a90ae37a997b162edf028e61f40696bb
tavariuscode <hedertavares@gmail.com>
2025-06-10 21:00:14 -0300

    criar entidade orm

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,13 +2,17 @@
 
 declare(strict_types=1);
 
+
 namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
 
-
+#[ORM\Entity]
 class Category
 {
+    #[ORM\Id] #[ORM\Column] #[ORM\GeneratedValue]
     private  int $id;
 
+    #[ORM\Column(length: 25, nullable: true)]
     private string $name;
 
     private string $description;
