1d9023f0f864ae94bff12f41818b88bffaab40ac
Jorge Eduardo Andrade Siqueira <jorgedigitalcollege@outlook.com>
2025-06-17 20:32:26 -0300

    refactor: ajustar arquivos de rotas conforme issue #112 (#119)

--- routes/advertisementRoutes.php
+++ routes/advertisementRoutes.php
@@ -1,7 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Controller\AdvertisementController;
-use App\Controller\UserController;
 
 return [
     '/anuncios' => [AdvertisementController::class, 'list'],
