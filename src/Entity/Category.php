92f850820b92f4fde0a82eb9fefb35e2d488a256
Allyson Gerald <allyson.gerald@gmail.com>
2025-06-17 21:03:41 -0300

    refactor: Realizado melhorias entidade categoria e informação contatos (#127)

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,27 +2,71 @@
 
 declare(strict_types=1);
 
-
 namespace App\Entity;
+
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
 class Category
 {
     #[ORM\Id] #[ORM\Column] #[ORM\GeneratedValue]
-    private  int $id;
+    private int $id;
 
     #[ORM\Column(length: 25, nullable: true)]
     private string $name;
 
+    #[ORM\Column]
     private string $description;
 
+    #[ORM\Column]
     private DateTime $createdAt;
 
+    #[ORM\Column]
     private DateTime $updatedAt;
 
-   
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
 }
-
-
-
